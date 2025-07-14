b64fc6fb26b26f07275cb84dee0c0dd80389a02f
Roottoot <syuleiman@gmail.com>
2025-07-14 11:40:41 +0200

    New commit Feature A

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Change from feature A");
-        System.out.println("New changes");
-
+        System.out.println("Change from pull request A");
     }
 }
